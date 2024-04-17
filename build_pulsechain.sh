aabb1321e46ffac945907b91a99ceb86d23746d8
Ilya Taldykin <ilya.taldykin@redduck.io>
2024-04-17 11:26:17 +0000

    feat: new addresses

--- build_pulsechain.sh
+++ build_pulsechain.sh
@@ -7,5 +7,5 @@ ARAGON_WALLETCONNECT_RPC_URL=https://rpc.v4.testnet.pulsechain.com \
 ARAGON_APP_LOCATOR=ipfs \
 ARAGON_ETH_NETWORK_TYPE=pulsechain \
 ARAGON_ENS_REGISTRY_ADDRESS=0x214DCdEF239b4C5AB4cd2d5185d7964170E95251 \
-TAG=pulsechain ./build.sh
+TAG=pulsechain bash build.sh
 
